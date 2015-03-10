06457487e1f67ca1435edc8634a5c1261d463253
karolmajek <karolmajek@gmail.com>
2015-03-10 22:10:38 +0100

    remove windows_bat

--- windows_bat/filtering_hannover2.bat
+++
@@ -1,3 +0,0 @@
-cd ..\BUILD\Release\
-filtering.exe C:\Users\karol\Desktop\data_registration_pcl\DATASETS\hannover2\modelOdom_0-10.xml C:\Users\karol\Desktop\data_registration_pcl\windows_bat\hannover_model_filtered.xml -s 2.0 -k 60
-pause
